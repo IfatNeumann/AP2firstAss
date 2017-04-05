20954498c2f6a98298a11f73f3b89ba9ba437865
ifat neumann <ineumann19@gmail.com>
2017-04-05 10:48:30 +0300

    hi

---
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/Searcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    public abstract class Searcher : ISearcher
+    {
+        private MyPriorityQueue<State> openList;
+        private intevaluatedNodes;
+        publicSearcher()
+        {
+            openList = new MyPriorityQueue<State>();
+            evaluatedNodes = 0;
+        }
+        protected State popOpenList()
+        {
+            evaluatedNodes++;
+            return openList.poll();
+        }
+    }
+}
