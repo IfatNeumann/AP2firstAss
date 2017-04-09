88371ffb21ff7ad07c695f0878a09258c3fb57d0
ifat neumann <ineumann19@gmail.com>
2017-04-09 15:15:26 +0300

    k

--- SearchAlgorithmsLib/SearchAlgorithmsLib/BFS.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/BFS.cs
@@ -28,21 +28,13 @@ namespace SearchAlgorithmsLib
             while (OpenListSize > 0)
             {
                 State<T> n = popDataStructor(); // inherited from Searcher, removes the best state
-                Console.WriteLine("the point:" + n.myState.ToString());
                 closed.Add(n);
-                Console.Write("\t in closed: ");
-                foreach (State<T> s1 in closed)
-                {
-                    Console.Write(s1.myState.ToString());
-                }
                 if (n.Equals(searchable.getGoalState()))
                     return n.backTrace(); // private method, back traces through the parents
                 // calling the delegated method, returns a list of states with n as a parent
                 List<State<T>> succerssors = searchable.getAllPossibleStates(n);
-                Console.WriteLine("\n num of neighbors:" + succerssors.Count.ToString());
                 foreach (State<T> s in succerssors)
                 {
-                    Console.WriteLine("neighbor:" + s.myState.ToString());
                     foreach (State<T> s1 in closed)
                     {
                         if (s.myState.Equals(s1.myState))
@@ -76,13 +68,7 @@ namespace SearchAlgorithmsLib
                     inOpenList = false;
                     inClosed = false;
                 }
-                Console.Write("\n\t in open: ");
-                foreach (State<T> s1 in openList)
-                {
-                    Console.Write(s1.myState.ToString());
-                }
                 OpenListSize = openList.Count;
-                Console.WriteLine("\n open list size:" + openList.Count.ToString());
             }
             return null;
         }
