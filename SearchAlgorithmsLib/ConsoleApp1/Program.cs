1c6514734f40aa1b34f1ff7a4cf51f5a0bf4ecb4
ifat neumann <ineumann19@gmail.com>
2017-04-09 09:00:16 +0300

    started the second part of mission A

---
+++ SearchAlgorithmsLib/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        void CompareSolvers()
+        {
+            //create maze
+            //print maze
+            //BFS solution
+            //DFS solution
+            //print num of stages
+        }
+    }
+}
