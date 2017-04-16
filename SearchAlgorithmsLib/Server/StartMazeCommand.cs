245b4ea7cf2da6564e6724f364059fc6f33059fe
m1245 <m1245@DESKTOP-VVLE821>
2017-04-16 10:59:31 +0300

    ll

--- SearchAlgorithmsLib/Server/StartMazeCommand.cs
+++ SearchAlgorithmsLib/Server/StartMazeCommand.cs
@@ -15,12 +15,102 @@ namespace Server
         {
             this.model = model;
         }
+        private int numOfUsers = 2;
+        private Socket client1;
+        private Socket client2;
+        private string name;
+        private string maze1;
+        private string maze2;
+
+        public int NumOfUsers
+        {
+            get
+            {
+                return numOfUsers;
+            }
+
+            set
+            {
+                numOfUsers = value;
+            }
+        }
+        public Socket Client1
+        {
+            get
+            {
+                return client1;
+            }
+
+            set
+            {
+                client1 = value;
+            }
+        }
+        public Socket Client2
+        {
+            get
+            {
+                return client2;
+            }
+
+            set
+            {
+                client2 = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public string Maze1
+        {
+            get
+            {
+                return maze1;
+            }
+
+            set
+            {
+                maze1 = value;
+            }
+        }
+        public string Maze2
+        {
+            get
+            {
+                return maze2;
+            }
+
+            set
+            {
+                maze2 = value;
+            }
+        }
+
+
         public string Execute(string[] args, TcpClient client)
         {
-            string name = args[0];
+            List<string> mazes = model.NamesList();
+            Random rand = new Random();
+
             int rows = int.Parse(args[1]);
             int cols = int.Parse(args[2]);
-            return "hi";
+            Maze maze = model.GenerateMaze(name, rows, cols);
+
+            int k = maze.Rows + maze.Cols;
+            string n = "kk";
+            return n;
+
+
+
         }
     }
 }
