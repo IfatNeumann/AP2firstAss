5fddc9add86e1ae7f85b98d4b9b639f4a7e5394e
ifat neumann <ineumann19@gmail.com>
2017-05-05 15:54:06 +0300

    settings working!

--- SearchAlgorithmsLib/WPFGame/MainWindowVM.cs
+++ SearchAlgorithmsLib/WPFGame/MainWindowVM.cs
@@ -14,36 +14,5 @@ namespace WPFGame
         {
             this.model = model;
         }
-
-        public string ServerIP
-        {
-            get
-            {
-                return this.model.ServerIP;
-            }
-            set
-            {
-                this.model.ServerIP = value;
-                this.NotifyPropertyChanged("ServerIP");
-            }
-        }
-
-        public int ServerPort
-        {
-            get
-            {
-                return this.model.ServerPort;
-            }
-            set
-            {
-                this.model.ServerPort = value;
-                this.NotifyPropertyChanged("ServerPort");
-            }
-        }
-
-        public void SaveSettings()
-        {
-            this.model.SaveSettings();
-        }
     }
 }
