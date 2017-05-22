e16c5c437f1f51949734fe6e57647b843a2109bf
m1245 <m1245@DESKTOP-VVLE821>
2017-05-22 21:55:09 +0300

    m

--- SearchAlgorithmsLib/WPFGame/MazeUserControl.xaml.cs
+++ SearchAlgorithmsLib/WPFGame/MazeUserControl.xaml.cs
@@ -154,8 +154,8 @@ namespace WPFGame
             this.playerRec = new Rectangle();
             this.playerRec.Width = this.rectWidth;
             this.playerRec.Height = this.rectHeight;
-            Canvas.SetLeft(this.playerRec, start.Y * this.rectWidth);
-            Canvas.SetTop(this.playerRec, start.X * this.rectHeight);
+            Canvas.SetLeft(this.playerRec, start.X * this.rectWidth);
+            Canvas.SetTop(this.playerRec, start.Y * this.rectHeight);
             this.playerRec.Fill = marco;
             this.MyCanvas.Children.Add(this.playerRec);
             this.rectList.Add(this.playerRec);
@@ -185,6 +185,8 @@ namespace WPFGame
             int i = 0 , length = solution.Length;
             int x = this.myMaze.InitialPos.Col;
             int y = this.myMaze.InitialPos.Row;
+            Canvas.SetLeft(this.playerRec, x * this.rectWidth);
+            Canvas.SetTop(this.playerRec, y * this.rectHeight);
             while (i < length)
             {
                 switch (solution[i])
