e599e16a5dca78ed45b90c9f878282571cc7b7bc
ifat neumann <ineumann19@gmail.com>
2017-05-24 01:10:52 +0300

    players

--- SearchAlgorithmsLib/WPFGame/WaitWindow.xaml.cs
+++ SearchAlgorithmsLib/WPFGame/WaitWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace WPFGame
     {
         public WaitWindow()
         {
-           // InitializeComponent();
+            this.InitializeComponent();
         }
 
         private void Back_To_Main_Button_Click(object sender, RoutedEventArgs e)
