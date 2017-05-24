890c3c9d4494cd2fcfa9589edda47b6038ca2df0
m1245 <m1245@DESKTOP-VVLE821>
2017-05-24 23:43:42 +0300

    comments

--- SearchAlgorithmsLib/WPFGame/SinglePlayer/SinglePlayer.xaml.cs
+++ SearchAlgorithmsLib/WPFGame/SinglePlayer/SinglePlayer.xaml.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace WPFGame
 {
     /// <summary>
     /// Interaction logic for SinglePlayer.xaml
     /// </summary>
+    /// <seealso cref="System.Windows.Window" />
+    /// <seealso cref="System.Windows.Markup.IComponentConnector" />
     public partial class SinglePlayer : Window
     {
+        /// <summary>
+        /// The vm of the single player
+        /// </summary>
         private SinglePlayerViewModel vm;
+        /// <summary>
+        /// The model
+        /// </summary>
         private ISinglePlayerModel model;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SinglePlayer"/> class.
+        /// </summary>
         public SinglePlayer()
         {
             this.model = new ApplicationSinglePlayerModel();
@@ -30,7 +30,12 @@ namespace WPFGame
             this.DataContext = this.vm;
 
         }
-        
+
+        /// <summary>
+        /// Handles the Click event of the Start_Button control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void Start_Button_Click(object sender, RoutedEventArgs e)
         {
             this.model.StartGame();
