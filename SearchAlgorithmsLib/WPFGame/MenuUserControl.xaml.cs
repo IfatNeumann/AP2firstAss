3b59ab9d315c968ee753cf425c9e05e37d7ea76d
m1245 <m1245@DESKTOP-VVLE821>
2017-05-24 23:18:57 +0300

    commit start comments

--- SearchAlgorithmsLib/WPFGame/MenuUserControl.xaml.cs
+++ SearchAlgorithmsLib/WPFGame/MenuUserControl.xaml.cs
@@ -18,8 +18,13 @@ namespace WPFGame
     /// <summary>
     /// Interaction logic for MenuUserControl.xaml
     /// </summary>
+    /// <seealso cref="System.Windows.Controls.UserControl" />
+    /// <seealso cref="System.Windows.Markup.IComponentConnector" />
     public partial class MenuUserControl : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuUserControl"/> class.
+        /// </summary>
         public MenuUserControl()
         {
             InitializeComponent();
