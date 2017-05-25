a5cc212d19fcc3124e308646f37b1619fd869db6
m1245 <m1245@DESKTOP-VVLE821>
2017-05-25 22:26:20 +0300

    new window

---
+++ SearchAlgorithmsLib/WPFGame/OtherWindows/CheckArgsWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFGame.OtherWindows
+{
+    /// <summary>
+    /// Interaction logic for CheckArgsWindow.xaml
+    /// </summary>
+    public partial class CheckArgsWindow : Window
+    {
+        public CheckArgsWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Back_To_Main_Button control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void Back_To_Main_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win = new MainWindow();
+            win.Show();
+            this.Close();
+        }
+    }
+}
